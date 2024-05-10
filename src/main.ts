4a30de2c8e546698f2d69f842e3fec6043e67d83
apacz1 <112419536+apacz1@users.noreply.github.com>
2024-05-10 18:31:20 +0200

    added popout window to insert tasks into

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,23 @@
+import { addItems } from "./utility/additems";
 import { newList } from "./utility/addlist";
+import { removeList } from "./utility/removelist";
+import { selectList } from "./utility/selectlist";
 
 const addBtn = document.querySelector(".addbtn");
+const removeBtn = document.querySelector(".removebtn");
+const listItem = document.querySelector(".list-item");
+const addTaskBtn = document.querySelector(".addtask");
+const popup = document.querySelector(".popup");
 
 addBtn?.addEventListener("click", newList);
+removeBtn?.addEventListener("click", removeList);
+listItem?.addEventListener("click", selectList);
+addTaskBtn?.addEventListener("click", addItems);
+
+document.body.addEventListener("click", (e: any) => {
+  let clickInside = popup?.contains(e.target);
+  let insideButton = addTaskBtn?.contains(e.target);
+  if (!clickInside && !insideButton) {
+    popup?.classList.remove("show");
+  }
+});
